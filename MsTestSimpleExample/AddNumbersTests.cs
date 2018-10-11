a6045dd11c765327608e8a41d8bc74166b91e5f3
Siarhei Mahlysh <maglish@mail.ru>
2018-10-11 17:09:06 +0300

    Initial commit

---
+++ MsTestSimpleExample/AddNumbersTests.cs
@@ -0,0 +1,25 @@
+﻿namespace MsTestSimpleExample
+{
+    using System.Threading;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AddNumbersTests
+    {
+        [TestMethod]
+        [TestCategory("AddNumbers")]
+        public void AddTwoIntNumbersTest()
+        {
+            Assert.AreEqual(5 + 5, 10);
+        }
+
+        [TestMethod]
+        [TestCategory("AddNumbers")]
+        public void AddTwoDoubleNumbersTest()
+        {
+            Thread.Sleep(6000);
+            Assert.AreEqual(5.5 + 5.25, 10.75);
+        }
+    }
+}
---
+++ MsTestSimpleExample/SubtractNumbersTests.cs
@@ -0,0 +1,23 @@
+﻿namespace MsTestSimpleExample
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SubtractNumbersTests
+    {
+        [TestMethod]
+        [TestCategory("SubNumbers")]
+        public void AddTwoIntNumbersTest()
+        {
+            Assert.AreEqual(5 - 4, 1);
+            Microsoft.VisualStudio.Coverage
+        }
+
+        [TestMethod]
+        [TestCategory("SubNumbers")]
+        public void AddTwoDoubleNumbersTest()
+        {
+            Assert.AreEqual(5.5 - 5.25, 0.25);
+        }
+    }
+}
